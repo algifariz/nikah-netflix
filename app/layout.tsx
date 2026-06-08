77103988406075849fefa9df3f6784c6b591233a
algifariz <algifari.zidan19@gmail.com>
2026-06-08 20:09:30 +0700

    Enhance Open Graph metadata structure by adding detailed properties for improved social media sharing

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,27 @@ export async function generateMetadata(): Promise<Metadata> {
       template: '%s | Wedding Invitation',
     },
     description: 'You are invited to our wedding celebration',
+    openGraph: {
+      title: 'Wedding Invitation',
+      description: 'You are invited to our wedding celebration',
+      siteName: 'Wedding Invitation',
+      locale: 'id_ID',
+      type: 'website',
+      images: [
+        {
+          url: '/og-image.jpg',
+          width: 1200,
+          height: 630,
+          alt: 'Wedding Invitation',
+        },
+        {
+          url: '/og-image-square.jpg',
+          width: 400,
+          height: 400,
+          alt: 'Wedding Invitation',
+        },
+      ],
+    },
   }
 }
 
